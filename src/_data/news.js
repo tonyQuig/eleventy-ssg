9cc60cd6d7491f02ff7a219b6c552da762a04292
tonyquig <tquigley9@gmail.com>
2020-03-21 11:34:50 +0000

    Trying to get netlify to write redirect rules..

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,23 +1,27 @@
 const axios = require("axios");
-require ('dotenv').config()
-const countries = require("./countries.json")
+const countries = require("./countries.json");
+
+require('dotenv').config();
 
 async function getNews(country) {
   try {
-    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`)
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`);
     return {
-      "country": country,
-      "articles": response.data.articles
+     "country": country,
+     "articles": response.data.articles
     }
   } catch (error) {
-    console.log('Error: ', error)
+    console.error(error);
   }
 }
 
+
 module.exports = async function() {
 
-  var newsPromises = countries.map(getNews);
-  return Promise.all(newsPromises).then((newsObjects) => {
-    return [].concat.apply([], newsObjects)
-  })
-}
+    var newsPromises = countries.map(getNews);
+    return Promise.all(newsPromises).then( newsObjects => {
+      console.log('newsObjects:', newsObjects);
+      return [].concat.apply([],newsObjects);
+    });
+
+};
