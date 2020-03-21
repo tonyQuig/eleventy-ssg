6a5f2e8fd2e0968089f686e59607cbb21ed08521
tonyquig <tquigley9@gmail.com>
2020-03-21 11:12:43 +0000

    Added netlify redirects file. Added countries JSON in order to pull news stories based on user country.

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,11 +1,23 @@
 const axios = require("axios");
 require ('dotenv').config()
+const countries = require("./countries.json")
 
-module.exports = async function() {
+async function getNews(country) {
   try {
-    const response = await axios.get(`http://newsapi.org/v2/everything?q=bitcoin&from=2020-02-17&sortBy=publishedAt&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`)
-    return response.data;
+    const response = await axios.get(`http://newsapi.org/v2/top-headlines?country=${country}&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`)
+    return {
+      "country": country,
+      "articles": response.data.articles
+    }
   } catch (error) {
     console.log('Error: ', error)
   }
+}
+
+module.exports = async function() {
+
+  var newsPromises = countries.map(getNews);
+  return Promise.all(newsPromises).then((newsObjects) => {
+    return [].concat.apply([], newsObjects)
+  })
 }
